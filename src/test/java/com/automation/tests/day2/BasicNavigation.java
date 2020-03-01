6bdc6e7a7952beec2466488e87e828fb6ab4b33c
brianingco <brian.ingco@yahoo.com>
2020-03-01 14:11:14 -0700

    QuitVsClose

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -1,28 +1,72 @@
 package com.automation.tests.day2;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import javax.lang.model.SourceVersion;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 public class BasicNavigation {
-
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception{
         //to start selenium script we need:
-        //setup webDriver (browser driver) and create webDriver object
+        //setup webdriver (browser driver) and create webdriver object
         WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver();
-        //in selenium, everything starts from WebDriver interface
-        //ChromeDriver extends RemoteWebDriver  --> implements WebDriver
+        WebDriver driver = new ChromeDriver();
+        //In selenium, everything starts from WebDriver interface'
+        //ChromeDriver extends RemoteWebDriver --> implements WebDriver
 
-        driver.get("http://google.com");  //to open a website
+        driver.get("http://google.com");//to open a website
+        driver.manage().window().maximize();//to maximize browser
+//        driver.manage().window().fullscreen();
+        Thread.sleep(3000);//for demo, wait 3 seconds
+        //method that return page title
+        //you can also see it as tab name, in the browser
 
-        Thread.sleep(3000);   //for demo to wait 3 seconds
-        String title = driver.getTitle();  //returns <title> Some title<t/title>
-        System.out.println("Title = " + title);
+        String title = driver.getTitle();//returns <title>Some title</title> text
+        String expectedTitle = "Google";//we provide it
+        System.out.println("Title is..."+title);
+
+        if(expectedTitle.equals(title)){
+            System.out.println("TEST PASSED!");
+        }else{
+            System.out.println("TEST FAILED!");
+        }
 
 
-        driver.close();  //to close browser
+        //go to another website within the same window
+        driver.navigate().to("http://amazon.com");
+        if(driver.getTitle().toLowerCase().contains("amazon")){
+            System.out.println("TEST PASSED!");
+        }else{
+            System.out.println("TEST FAILED!");
+        }
 
+        //comeback to google
+        driver.navigate().back();
+        Thread.sleep(3000);
+
+        //Checking if page title is equals to Google
+        verifyEquals(driver.getTitle(), "Google");
+
+        driver.navigate().forward();
+        System.out.println("Title = " + driver.getTitle());
+        System.out.println("URL: " + driver.getCurrentUrl());   //to get URL
+
+        driver.navigate().refresh();   //to reload page
+        Thread.sleep(3000);   //wait 3 seconds
+
+
+        //must be at the end
+        driver.close();//to close browser
+        //browser cannot close itself
+    }
+    //Check if two strings are the same.  If so print Test passed
+    //
+    public static void verifyEquals(String arg1, String arg2){
+        if(arg1.equals(arg2)){
+            System.out.println("TEST PASSED!");
+        }else{
+            System.out.println("TEST FAILED");
+        }
     }
 }
