2636c60fa1dfdc5974d35f2ca81ca7aa6f868e44
brianingco <brian.ingco@yahoo.com>
2020-03-07 09:47:56 -0700

    Quit vs Close

--- src/test/java/com/automation/tests/day5/radioButtons.java
+++ src/test/java/com/automation/tests/day5/radioButtons.java
@@ -1,8 +1,14 @@
 package com.automation.tests.day5;
 
+import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import javax.management.modelmbean.ModelMBeanInfoSupport;
+import java.util.List;
 
 public class radioButtons {
     public static void main(String[] args) throws InterruptedException {
@@ -12,6 +18,30 @@ public class radioButtons {
 
         driver.get("http://practice.cybertekschool.com/radio_buttons");
         driver.manage().window().maximize();
-        Thread.sleep(2000);
+        BrowserUtils.wait(2);
+
+        //<input type="radio">
+        List<WebElement> radioButtons = driver.findElements(By.tagName("input"));
+
+
+        for(WebElement radioButton: radioButtons){
+
+            String id = radioButton.getAttribute("id");
+
+            if(radioButton.isEnabled()){
+                radioButton.click();
+                System.out.println("Clicked on:" + radioButton.getAttribute("id"));
+                Thread.sleep(1000);
+            } else {
+                System.out.println("Button is disabled");
+            }
+
+        }
+
+
+
+
+
+        driver.quit();
     }
 }
