34b557d98330faf957c68dddb13d0104a1be2bd4
brianingco <brian.ingco@yahoo.com>
2020-03-07 09:19:08 -0700

    Quit vs Close

--- src/test/java/com/automation/tests/day3/FindElementById.java
+++ src/test/java/com/automation/tests/day3/FindElementById.java
@@ -24,6 +24,9 @@ public class FindElementById {
         driver.findElement(By.id("wooden_spoon")).click();
         Thread.sleep(2000);
 
+
+
+
         String expected = "Welcome to the Secure Area. When you are done click logout below.";
         String actual = driver.findElement(By.tagName("h4")).getText();
         if(expected.equals(actual)){
---
+++ src/test/java/com/automation/tests/day5/radioButtons.java
@@ -0,0 +1,17 @@
+package com.automation.tests.day5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class radioButtons {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+    }
+}
