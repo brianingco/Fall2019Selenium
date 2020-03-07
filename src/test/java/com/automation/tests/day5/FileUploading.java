180ef1b735fa1bcb8b1d44e144277965fb04615e
brianingco <brian.ingco@yahoo.com>
2020-03-07 15:57:03 -0700

    Quit vs Close

---
+++ src/test/java/com/automation/tests/day5/FileUploading.java
@@ -0,0 +1,36 @@
+package com.automation.tests.day5;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FileUploading {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromiumdriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/upload");
+        Thread.sleep(5000);
+
+        WebElement upload = driver.findElement(By.id("file-upload"));
+        String filePath = System.getProperty("user.dir")+ "/pom.xml";
+        System.out.println(filePath);
+
+
+        upload.sendKeys(filePath);
+
+        driver.findElement(By.id("file-submit")).click();
+
+        BrowserUtils.wait(5);
+
+
+
+
+        driver.quit();
+    }
+}
