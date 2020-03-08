2fa96dfdeb05c3118acaf631c57801d82a4e9b85
brianingco <brian.ingco@yahoo.com>
2020-03-08 11:17:00 -0600

    Quit vs Close

--- src/test/java/com/automation/tests/day6/NoSelectDropdown.java
+++ src/test/java/com/automation/tests/day6/NoSelectDropdown.java
@@ -1,4 +1,33 @@
 package com.automation.tests.day6;
 
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.WebDriver;
+
 public class NoSelectDropdown {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = DriverFactory.createADriver("chrome");
+        driver.get("http://practice.cybertekschool.com/dropdown");
+        BrowserUtils.wait(3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        BrowserUtils.wait(5);
+        driver.quit();
+
+
+    }
+
 }
